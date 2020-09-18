7cfb6bf41761b32ca1882d946b46ad17195446f3
anthonyrodarte <anthonyrodarte1422@gmail.com>
2020-09-17 22:07:05 -0700

    Created OpenLayers map.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 
 import './App.css';
 import Header from './components/Header';
-import Counter from './components/Counter'
-import Notepad from './components/Notepad'
+import Counter from './components/Counter';
+import Notepad from './components/Notepad';
+import OLMap from './components/Map'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Counter />
+      <OLMap />
       <Notepad />
+      <Counter />
     </div>
 
   );
