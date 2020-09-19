d48ebce32daebacb961ebf1fc68c123d935f1def
iameddiejl <edwardjlopez92@gmail.com>
2020-09-18 23:50:08 -0700

    okidoki

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './components/Header';
 import Counter from './components/Counter'
 import Notepad from './components/Notepad'
+import Forecast from './components/Forecast'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Counter />
       <Notepad />
+      <Forecast />
     </div>
 
   );
