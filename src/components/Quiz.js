37b7c4f17b6458ac59a18f643eb0db189550ec9b
anthonyrodarte <anthonyrodarte1422@gmail.com>
2021-01-10 00:03:59 -0800

    Keeping track of Ingredient List input changes

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { first } from 'lodash'
 
 import Cocktails from './Cocktails'
 
@@ -7,27 +9,75 @@ import Cocktails from './Cocktails'
 const Quiz = () => {
 
   const [cocktailList, changeCocktailList] = useState(Cocktails)
+  const [ingredientInputs, changeIngredientInput] = useState([{quantity: '', measurement: '', ingredient: ''}])
 
 
-  const {name: cocktailName, recipe: cocktailRecipe} = cocktailList[0]
+  const {name: cocktailName, recipe: cocktailRecipe} = first(cocktailList)
 
   const updateCocktailList  = () => {
-    const [first, ...rest] = cocktailList
-
-    changeCocktailList([...rest, first])
+    const [oldCocktail, ...rest] = cocktailList
+    changeCocktailList([...rest, oldCocktail])
   }
 
+  const addIngredientInput = (e) => {
+    e.preventDefault()
+    changeIngredientInput([...ingredientInputs, {quantity: '', measurement: '', ingredient: ''}])
+  }
+
+  const updateInputValues = (e) => {
+    const i = e.target.getAttribute('data-index')
+    const type = e.target.getAttribute('data-type')
+    const { value } = e.target
+
+    let inputs = [...ingredientInputs]
+    let input = inputs[i]
+
+    switch (type) {
+      case 'quantity':
+        input.quantity = value
+        inputs[i] = input
+        break;
+      case 'measurement':
+        input.measurement = value
+        inputs[i] = input
+        break;
+      case 'ingredient':
+        input.ingredient = value
+        inputs[i] = input
+        break;
+      default:
+        return
+    }
+
+    changeIngredientInput(inputs)
+  }
+  
+  console.log('checkstate', ingredientInputs)
   return (
     <div>
-      <span>{cocktailName}</span>
+      <p>{cocktailName}</p>
+      <form>
+        <label>Ingredient List
+        <br />
+        {ingredientInputs.map((ingredients, idx) => (
+          <div key={idx} >
+            <input data-index={idx} data-type='quantity' placeholder="quantity" onChange={updateInputValues} value={ingredients.quantity}/>
+            <select data-index={idx} data-type='measurement' onChange={updateInputValues}>
+              <option value=""></option>
+              <option value="oz">oz</option>
+              <option value="dashes">dashe(s)</option>
+            </select>
+            <input data-index={idx} data-type='ingredient' placeholder="ingredient" onChange={updateInputValues}/>
+          </div>
+        ))}
+        <br />
+        <button onClick={addIngredientInput}>+</button>
+        <br />
+        <button onClick={updateCocktailList}>Submit</button>
+        </label>
+      </form>
 
-      <ul>
-        {cocktailRecipe.map((ingredient, idx) => (
-        <li key={idx}>{ingredient}</li>
-      ))}
-      </ul>
-
-      <button onClick={updateCocktailList}>Change Cocktail</button>
+      
     </div>
   )
 }
